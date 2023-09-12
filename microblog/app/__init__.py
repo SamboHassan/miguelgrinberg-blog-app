25458555d3be463340f0fd4e5b930fcba576f1c0
Hassan Sambo <sambohasan@gmail.com>
2023-09-12 21:03:03 +0100

    add base template file

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from config import Config
 
 app = Flask(
     __name__, template_folder="/home/soft-dev/blog-app-miguel/microblog/templates"
 )
+app.config.from_object(Config)
 
 from app import routes
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, redirect, flash
 from app import app
+from app.forms import LoginForm
 
 
 @app.route("/")
@@ -13,3 +14,16 @@ def index():
         {"author": {"username": "Sambo"}, "body": "Muhammad is the messenger of God"},
     ]
     return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            "Login requested for user {}, remember_me={}".format(
+                form.username.data, form.remember_me.data
+            )
+        )
+        return redirect("/index")
+    return render_template("login.html", title="Sign In", form=form)
